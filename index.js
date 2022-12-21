168338062d15df67e8fbe141e9b535a570b6b7e3
Adir Elmaleh <adirelmaleh@gmail.com>
2022-12-22 01:54:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,232 @@
+const images1 = [
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368130_101_a-16686861648439675.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368130_101_b-16686861647372201.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368130_101_c-166868616410441157.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368130_101_d-166868616410601709.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368130_101_e-166868616412501370.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368130_101_f-166868616412141785.jpg',
+];
+const images2 = [
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368125_101_a-16686861647023956.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368125_101_b-16686861646493339.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368125_101_c-16686861649988324.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368125_101_d-16686861649599935.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368125_101_f-166868616410391382.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368125_101_e-166868616410572530.jpg',
+];
+const images3 = [
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/5/1/5169102_204_a-16479533492175497.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/5/1/5169102_204_c-16479533512707720.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/5/1/5169102_204_b-16479533502469785.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/5/1/5169102_204_d-16479533522989007.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/5/1/5169102_204_f-16479533553949848.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/5/1/5169102_204_e-16479533542867413.jpg',
+];
+const images4 = [
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368052_107_a-16580495012721426.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368052_107_c-16580495032762281.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368052_107_b-16580495023246222.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368052_107_d-16580495043258429.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368052_107_f-16580495073910999.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368052_107_e-16580495053983034.jpg',
+];
+const images5 = [
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368031_101_a-16589261591920788.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368031_101_c-16589261612344438.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368031_101_b-16589261601658450.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368031_101_d-16589261621913260.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368031_101_f-16589261642484561.jpg',
+    'https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/8/3/8368031_101_g-16589261652964439.jpg',
+];
+const images6 = [
+    "https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/1/1/1181_121_a-16632314792745260.jpg",
+    "https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/1/1/1181_121_b-16632314802798196.jpg",
+    "https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/1/1/1181_121_c-16632314823360086.jpg",
+    "https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/1/1/1181_121_d-16632314832889965.jpg",
+    "https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/1/1/1181_121_e-16632314853347487.jpg",
+    "https://www.golfco.co.il/pub/media/catalog/product/cache/a1ead2c2961e2ce13911c89cbb3a882f/1/1/1181_121_g-16632314872873328.jpg",
+];
+
+// quantity of items counter
+const minimalQuantity = 1;
+const maxQuantity = 10;
+
+let increase = document.getElementById("increment")
+let decrease = document.getElementById("decrement")
+let currentQuantity = document.getElementById("currentQuantity")
+let newQuantity = 1;
+
+
+increase.addEventListener('click', function () {
+    if (newQuantity < maxQuantity) {
+        newQuantity += 1;
+        currentQuantity.innerHTML = newQuantity;
+    }
+})
+
+decrease.addEventListener('click', function () {
+    if (newQuantity > minimalQuantity) {
+        newQuantity -= 1;
+        currentQuantity.innerHTML = newQuantity;
+    }
+})
+
+//heart button fill unfill
+let transparentHeart = document.querySelector('.heart-icon');
+let isFilled = false;
+transparentHeart.addEventListener('click', function () {
+    if (isFilled) {
+        transparentHeart.innerHTML = "&#9825";
+        isFilled = false;
+    }
+    else {
+        transparentHeart.innerHTML = "\u2764\uFE0F";
+        isFilled = true;
+    }
+})
+
+//min max price range
+const prices = [];
+let pricesOptions = document.getElementsByClassName('product-option-view');
+let pricesToNumber = [];
+let priceRange = document.querySelector('.price-range');
+
+for (let index = 0; index < pricesOptions.length; index++) { //get prices into new array 
+    prices.push(pricesOptions[index].children[1].innerHTML)
+}
+
+prices.forEach(element => {
+    pricesToNumber.push(parseFloat(element).toFixed(2)) // turn each string into a float
+});
+
+pricesToNumber.sort(function (a, b) { return a - b }); // sort numbers array min to max
+
+priceRange.children[0].innerHTML = pricesToNumber[pricesToNumber.length - 1] + ' ' + '₪' + '  - ';
+priceRange.children[1].innerHTML = pricesToNumber[0] + ' ' + '₪';
+
+
+
+// show selected price indication. change photos when selected, cancel price range and show selected price.
+let item = document.querySelector('.items')
+let currentTarget = 0;
+let isClicked = false;
+
+
+item.addEventListener('click', function onClick(e) {
+    if (isClicked) {
+        e.target.classList.add('selected');
+        currentTarget.classList.remove('selected');
+        currentTarget = e.target;
+        priceRange.children[0].innerHTML = currentTarget.children[1].innerHTML
+        changePhoto(currentTarget.getAttribute('data-id'));
+    }
+    else {
+        e.target.classList.add('selected');
+        currentTarget = e.target;
+        isClicked = true;
+        priceRange.children[1].remove()
+        priceRange.children[0].innerHTML = currentTarget.children[1].innerHTML
+        changePhoto(currentTarget.getAttribute('data-id'));
+    }
+})
+
+
+
+
+function changePhoto(number) {
+    let gallery = document.querySelector('.grid-gallery').getElementsByTagName('img')
+    gallery = Array.prototype.slice.call(gallery);
+
+    let i = 0;
+    switch (number) {
+        case '1':
+            gallery.forEach(img => img.src = images1[i++])
+            break;
+
+        case '2':
+            gallery.forEach(img => img.src = images2[i++])
+            break;
+
+        case '3':
+            gallery.forEach(img => img.src = images3[i++])
+            break;
+
+        case '4':
+            gallery.forEach(img => img.src = images4[i++])
+            break;
+
+        case '5':
+            gallery.forEach(img => img.src = images5[i++])
+            break;
+
+        case '6':
+            gallery.forEach(img => img.src = images6[i++])
+            break
+    }
+    i = 0;
+}
+//carousel mobile devices
+
+const carousel = document.querySelector('.carousel');
+let firstImg = carousel.querySelectorAll('img')[0];
+let isDragStart = false, prevPageX, prevScrollLeft, positionDiff;
+
+const autoSlide = () => {
+    positionDiff = Math.abs(positionDiff);
+    let firstImgWidth = firstImg.clientWidth + 4;
+    let valDifference = firstImgWidth - positionDiff;
+    if (carousel.scrollLeft > prevScrollLeft) {
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add('dragging');
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove('dragging');
+    autoSlide();
+}
+carousel.addEventListener('touchstart', dragStart);
+carousel.addEventListener('touchmove', dragging);
+carousel.addEventListener('touchend', dragStop);
+
+//tab
+let i, tabContentItem, tabContentReturn, deliveryContent, productContent;
+
+tabContentReturn = document.getElementById("1");
+deliveryContent = document.getElementById("return-content");
+tabContentItem = document.getElementById("0");
+productContent = document.getElementById("product-content");
+
+tabContentReturn.addEventListener('click', (e) => {
+    tabContentReturn.classList.add('title');
+    deliveryContent.classList.add('active');
+    deliveryContent.classList.remove('not-active');
+    tabContentItem.classList.remove('title');
+    productContent.classList.remove('active');
+    productContent.classList.add('not-active');
+
+})
+
+tabContentItem.addEventListener('click', (e) => {
+    tabContentItem.classList.add('title');
+    productContent.classList.add('active');
+    productContent.classList.remove('not-active');
+    tabContentReturn.classList.remove('title');
+    deliveryContent.classList.remove('active');
+    deliveryContent.classList.add('not-active');
+})
+
